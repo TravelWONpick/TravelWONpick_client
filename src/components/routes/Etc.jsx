59cb030a301aec48f750380eb67d0910aa44d14a
gato-46 <qud406@gmail.com>
2024-11-01 15:20:09 +0900

    feat : 이벤트 페이지 수정

--- src/components/routes/Etc.jsx
+++ src/components/routes/Etc.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Etc = () => {
   return (
-    <div>Etc</div>
+      <div>Etc</div>
   )
 }
 
---
+++ src/components/routes/EventDetail.jsx
@@ -0,0 +1,23 @@
+// src/components/routes/EventDetail.jsx
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import '../../event.css';
+
+const EventDetail = () => {
+    const location = useLocation();
+    const { title, date, imgSrc } = location.state || {};
+
+    return (
+        <div className="event-detail">
+            {/* 제목과 이벤트 기간을 같은 행에 표시 */}
+            <div className="event-detail-header">
+                <h1 className="event-detail-title">{title}</h1>
+                <span className="event-detail-date">이벤트 기간: {date}</span>
+            </div>
+            <img src={imgSrc} alt={title} className="event-detail-image" />
+            <button className="event-detail-button">항공권 예약하러 가기</button>
+        </div>
+    );
+};
+
+export default EventDetail;
