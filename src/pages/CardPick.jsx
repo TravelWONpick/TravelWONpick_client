a91997831f8d3a2cbcfbba77b84069e351d09582
LeeYeonhee-00 <dldusgml0929@gachon.ac.kr>
2024-11-28 16:41:17 +0900

    fix : css 폴더로 분리

--- src/pages/CardPick.jsx
+++ src/pages/CardPick.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import api from '../components/axios';
 import { Tag, Button, Pagination } from 'antd';
 import { DownOutlined, UpOutlined } from '@ant-design/icons';
-import './CardPick.css';
+import '../css/CardPick.css';
 import useSessionClearOnMain from '../hooks/useSessionClearOnMain';
 
 const CardPick = () => {
