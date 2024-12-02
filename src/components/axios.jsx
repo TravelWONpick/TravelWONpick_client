a2ea1c05512228db3e97e9981e706f83cf0c2416
isshomin <zx149@naver.com>
2024-12-02 17:46:33 +0900

    fix : axios 컴포넌트 롤백

--- src/components/axios.jsx
+++ src/components/axios.jsx
@@ -1,33 +1,8 @@
-// // src/components/axios.js
-// import axios from 'axios';
-
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_APP_API_URL,
-//   });
-  
-//   api.interceptors.request.use(
-//     (config) => {
-//       const accessToken = sessionStorage.getItem('accessToken');
-//       if (accessToken) {
-//         config.headers['Authorization'] = `Bearer ${accessToken}`;
-//       }
-//       config.headers['Content-Type'] = 'application/json';
-//       return config;
-//     },
-//     (error) => {
-//       return Promise.reject(error);
-//     }
-//   );
-  
-
-// export default api;
-
-
 // src/components/axios.js
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_APP_API_URL}/api`,
+    baseURL: import.meta.env.VITE_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
     }
