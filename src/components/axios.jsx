48904b2f5503f41b14942c7447c15f5fdc6f715e
LeeYeonhee-00 <dldusgml0929@gachon.ac.kr>
2024-12-02 11:29:44 +0900

    fix : api 컴포넌트 수정

--- src/components/axios.jsx
+++ src/components/axios.jsx
@@ -4,8 +4,8 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: import.meta.env.VITE_APP_API_URL,
     headers: {
-        "Content-Type": "application/json",
-    },
+        'Content-Type': 'application/json',
+    }
 });
 
 export default api;
