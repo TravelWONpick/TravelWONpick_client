440cc6174316cb052c9855dcc9053753616483b1
LeeYeonhee-00 <dldusgml0929@gachon.ac.kr>
2024-12-02 17:18:51 +0900

    fix : api 요청 경로 수정

--- src/components/axios.jsx
+++ src/components/axios.jsx
@@ -1,23 +1,36 @@
+// // src/components/axios.js
+// import axios from 'axios';
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_APP_API_URL,
+//   });
+  
+//   api.interceptors.request.use(
+//     (config) => {
+//       const accessToken = sessionStorage.getItem('accessToken');
+//       if (accessToken) {
+//         config.headers['Authorization'] = `Bearer ${accessToken}`;
+//       }
+//       config.headers['Content-Type'] = 'application/json';
+//       return config;
+//     },
+//     (error) => {
+//       return Promise.reject(error);
+//     }
+//   );
+  
+
+// export default api;
+
+
 // src/components/axios.js
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL,
-  });
-  
-  api.interceptors.request.use(
-    (config) => {
-      const accessToken = sessionStorage.getItem('accessToken');
-      if (accessToken) {
-        config.headers['Authorization'] = `Bearer ${accessToken}`;
-      }
-      config.headers['Content-Type'] = 'application/json';
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+    baseURL: `${import.meta.env.VITE_APP_API_URL}/api`,
+    headers: {
+        'Content-Type': 'application/json',
     }
-  );
-  
+});
 
 export default api;
