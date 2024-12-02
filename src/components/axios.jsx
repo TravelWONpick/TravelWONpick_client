2a02285a23b33ad93ca8f44327bdcdd960810c34
isshomin <zx149@naver.com>
2024-12-02 19:07:15 +0900

    test : api 연속 호출 시 에러 확인 테스트

--- src/components/axios.jsx
+++ src/components/axios.jsx
@@ -5,6 +5,8 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache', // 캐시 비활성화
+        Pragma: 'no-cache', // HTTP 1.0 캐시 비활성화
     }
 });
 
