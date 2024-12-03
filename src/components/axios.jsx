7caea8395fd57b9781589307abde17baf8946d3b
isshomin <zx149@naver.com>
2024-12-03 19:42:54 +0900

    fix : axios interceptor 추가

--- src/components/axios.jsx
+++ src/components/axios.jsx
@@ -8,4 +8,21 @@ const api = axios.create({
     }
 });
 
+// 요청 인터셉터를 통해 토큰 추가
+api.interceptors.request.use(
+    (config) => {
+      // 세션 스토리지에서 accessToken 가져오기
+      const token = sessionStorage.getItem('accessToken');
+      if (token) {
+        // Authorization 헤더에 토큰 추가
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  
+
 export default api;
