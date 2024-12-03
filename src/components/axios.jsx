730510022e59136835911fec853805c0eb64bd74
isshomin <zx149@naver.com>
2024-12-03 16:51:51 +0900

    fix : baseURL 수정

--- src/components/axios.jsx
+++ src/components/axios.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL,
+    baseURL: `${import.meta.env.VITE_APP_API_URL}/api`,
     headers: {
         'Content-Type': 'application/json',
     },
