93072ba54ed8346d9b43be18e9cbd2929ff31176
isshomin <zx149@naver.com>
2024-12-03 10:37:35 +0900

    fix : authorization 에러 해결

--- src/components/axios.jsx
+++ src/components/axios.jsx
@@ -5,9 +5,18 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
-        'Cache-Control': 'no-cache', // 캐시 비활성화
-        Pragma: 'no-cache', // HTTP 1.0 캐시 비활성화
+    },
+});
+
+// 요청 인터셉터를 통해 토큰 추가
+api.interceptors.request.use((config) => {
+    const token = sessionStorage.getItem('accessToken');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default api;
